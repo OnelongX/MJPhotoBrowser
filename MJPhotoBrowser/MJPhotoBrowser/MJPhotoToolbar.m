0bca79fc72c13f8f38e447470b0453d9960011e5
Onelong <zzfeihua@163.com>
2018-10-15 15:11:22 +0800

    Update MJPhotoBrowser/MJPhotoBrowser/MJPhotoToolbar.m

--- MJPhotoBrowser/MJPhotoBrowser/MJPhotoToolbar.m
+++ MJPhotoBrowser/MJPhotoBrowser/MJPhotoToolbar.m
@@ -48,9 +48,10 @@
     CGFloat btnWidth = 24;//self.bounds.size.height;
     _saveImageBtn = [UIButton buttonWithType:UIButtonTypeCustom];
     
-    _saveImageBtn.frame = CGRectMake([UIScreen mainScreen].bounds.size.width  - 20, 0, btnWidth, btnWidth);
-    //_saveImageBtn.autoresizingMask = UIViewAutoresizingFlexibleHeight;
+    _saveImageBtn.frame = CGRectMake(200, 0, btnWidth, btnWidth);
+    _saveImageBtn.autoresizingMask = UIViewAutoresizingFlexibleHeight;
     //静态库
+    
     if([UIImage imageNamed:@"MJPhotoBrowser.bundle/save_icon.png"]) {
         [_saveImageBtn setImage:[UIImage imageNamed:@"MJPhotoBrowser.bundle/save_icon.png"] forState:UIControlStateNormal];
         [_saveImageBtn setImage:[UIImage imageNamed:@"MJPhotoBrowser.bundle/save_icon_highlighted.png"] forState:UIControlStateHighlighted];
